0de96981e0f4486af42a6aebcad28fc6163281e4
mehul <mehul.chopra.dev@gmail.com>
2022-03-14 11:59:08 +0530

    finished writing tests for todos app service

--- src/app/todos-app.service.spec.ts
+++ src/app/todos-app.service.spec.ts
@@ -10,7 +10,41 @@ describe('TodosAppService', () => {
     service = TestBed.inject(TodosAppService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('adds the todos properly', () => {
+    service.addTodo('todo 1');
+    service.addTodo('todo 2');
+
+    const todos = service.todos;
+    expect(todos.length).toBe(2);
+
+    const todo1 = todos[0];
+    expect(todo1.id).toBe(1);
+    expect(todo1.title).toBe('todo 1');
+    expect(todo1.done).toBeFalse();
+
+    const todo2 = todos[1];
+    expect(todo2.id).toBe(2);
+    expect(todo2.title).toBe('todo 2');
+    expect(todo2.done).toBeFalse();
   });
+
+  it('removes the todos properly', () => {
+    service.addTodo('todo 1');
+    service.addTodo('todo 2');
+    service.addTodo('todo 3');
+    service.addTodo('todo 4');
+
+    const todos = service.todos;
+
+    service.removeTodos([todos[0], todos[2]]);
+    let updatedTodos = service.todos;
+
+    expect(updatedTodos.length).toBe(2);
+    expect(updatedTodos[0].title).toBe('todo 2');
+    expect(updatedTodos[1].title).toBe('todo 4');
+
+    service.addTodo('todo 5');
+    updatedTodos = service.todos;
+    expect(updatedTodos[2].id).toBe(5);
+  });  
 });
