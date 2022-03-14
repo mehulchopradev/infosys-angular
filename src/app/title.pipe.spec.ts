e4ed10a571023f85def4e50b3cd3c615919b48f0
mehul <mehul.chopra.dev@gmail.com>
2022-03-14 11:24:48 +0530

    finished writing tests for title pipe

--- src/app/title.pipe.spec.ts
+++ src/app/title.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { TitlePipe } from './title.pipe';
 
 describe('TitlePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TitlePipe();
-    expect(pipe).toBeTruthy();
+
+  let pipe: TitlePipe;
+  beforeEach(() => {
+    pipe = new TitlePipe();
+  });
+
+  it('works with a single character input', () => {
+    const actualOutput = pipe.transform('r');
+    expect(actualOutput).toBe('R');
+  });
+
+  it('works with more than 1 character single word string', () => {
+    const actualOutput = pipe.transform('mEhUl');
+    expect(actualOutput).toBe('Mehul');
+  });
+
+  it('works with multi word multi character string', () => {
+    const actualOutput = pipe.transform('mehul chopra');
+    expect(actualOutput).toBe('Mehul chopra');
+  });
+
+  it('works with empty string passed in', () => {
+    const actualOutput = pipe.transform('');
+    expect(actualOutput).toBe('');
   });
 });
